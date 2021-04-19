7cabd83a79e42caefb723c30236d8234460d5ed2
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2021-04-19 16:17:17 +0300

    camera movement fix

---
+++ Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    [SerializeField] private float dragSpeed = 2;
+    private Vector3 pressedDownPoint;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pressedDownPoint = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0))
+            return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - pressedDownPoint);
+        Vector3 targetPosition = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+        transform.Translate(targetPosition, Space.World);
+    }
+}
